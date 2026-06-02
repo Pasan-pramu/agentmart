b1768e5c649e7dd929cc6b36c956f3987e136451
Pasan-pramu <pramudithapasan076@gmail.com>
2026-06-02 21:53:32 +0530

    feat: integrate Clerk authentication and add sign-in/sign-up routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,33 @@
-import { Link } from '@tanstack/react-router'
-import { LogIn } from 'lucide-react'
+import { Show, UserButton } from "@clerk/tanstack-react-start";
+import { Link } from "@tanstack/react-router";
+import { LogIn } from "lucide-react";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="brand">
-        <div className="mark">
-          <div className="glyph" />
-        </div>
+	return (
+		<nav className="navbar">
+			<div className="brand">
+				<div className="mark">
+					<div className="glyph" />
+				</div>
+				<Link to="/">
+					<span>AgentMart</span>
+				</Link>
+			</div>
 
-        <Link to="/">
-          <span>AgentMart</span>
-        </Link>
-      </div>
+			<div className="actions">
+				<Show when="signed-in">
+					<UserButton />
+				</Show>
 
-      <div className="actions">
-        <Link to="/sign-in/$" className="btn-primary">
-          <LogIn size={16} />
-          Sign in
-        </Link>
-      </div>
-    </nav>
-  )
-}
+				<Show when="signed-out">
+					<Link to="/sign-in/$" className="btn-primary">
+						<LogIn size={16} />
+						Sign in
+					</Link>
+				</Show>
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
